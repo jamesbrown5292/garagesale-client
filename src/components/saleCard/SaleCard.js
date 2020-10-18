89610032a27b6dcab91941b4021c34f80d9146bb
Vladyslav <kytsevolov@gmail.com>
2020-10-18 15:09:15 -0600

    basic card stylying

--- src/components/saleCard/SaleCard.js
+++ src/components/saleCard/SaleCard.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const SaleCard = () => (
-  <div className="sale-card-wrapper">
-    <img src="https://picsum.photos/300/210" alt="garage" />
-    <div className="sale-card__info">
-      <h4>Title</h4>
-      <p>Location</p>
+  <>
+    <div className="img-card-wrapper">
+      <img src="https://picsum.photos/300/210" alt="garage" />
     </div>
-  </div>
+    <div className="sale-card__info-position">
+      <div className="sale-card__info">
+        <h4>Title</h4>
+        <p>Location</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+  </>
 );
 
 export default SaleCard;
