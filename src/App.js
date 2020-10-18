4a5d7330c61baf8744bff415db1eb0ab9e22b697
Vladyslav <kytsevolov@gmail.com>
2020-10-18 17:15:05 -0600

    started drawer layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import { Container } from '@material-ui/core';
 import React from 'react';
-import SaleCardList from './components/saleCard/SaleCardList';
+import NavBar from './components/navbar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Container maxWidth="xl" component="div">
-        <h1>Welcome to Garage Sale App</h1>
-        <SaleCardList />
-      </Container>
+      <NavBar />
     </div>
   );
 }
